2f2820b7c40467afe6768f63cc486c7c0f200a3f
WTRIII <thomren3@gmail.com>
2021-12-06 23:16:18 -0600

    added footer with links

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Footer(){
+return(
+    <div> 
+
+<div>
+        <a href="https://github.com/WTRIII">
+            GitHub         
+        </a>
+      </div>
+
+      <div>
+        <a href="https://www.linkedin.com/in/william-t-renfroe-a146b065/">
+        Linked In
+        </a>
+      </div>
+
+      <div>
+        <a href="https://my.indeed.com/p/williamr-hxtyrl4">
+        Indeed
+        </a>
+      </div>
+
+      <div>
+        <a href="mailto:thomren3@gmail.com">
+        Email Me
+        </a>
+      </div>
+
+        
+    </div>
+)
+
+}
+
+export default Footer;
