8c04a12246957112b37788463408f438657410bc
WTRIII <thomren3@gmail.com>
2021-12-06 23:00:46 -0600

    updated portfolio with 3 more works

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,6 +2,9 @@ import React from 'react';
 import fedBuilder from '../images/fed-builder.PNG';
 import movieComparison from '../images/movie-comparison.PNG';
 import pipeDreams from '../images/pipe-dreams.PNG';
+import regexTutorial from '../images/regex-tutorial.PNG';
+import workout from '../images/Main-page.PNG';
+import techBlog from '../images/tech-blog-homepage.PNG';
 
 export default function Portfolio() {
   return (
@@ -28,7 +31,24 @@ export default function Portfolio() {
           <h2>Movie Comparison Site</h2>
         </a>
       </div>
-
+      <div>
+        <a href="https://gist.github.com/WTRIII/3a992ef519ac5fe01dac9071a6b9e785">
+          <img src={regexTutorial} alt="regex tutorial" />
+          <h2>Regular Expression Tutorial</h2>
+        </a>
+      </div>      
+      <div>
+        <a href="https://nosql-workout-tracker-wtriii.herokuapp.com/">
+          <img src={workout} alt="workout tracker" />
+          <h2>Movie Comparison Site</h2>
+        </a>
+      </div>      
+      <div>
+        <a href="https://mvc-tech-blog-wtriii.herokuapp.com/">
+          <img src={techBlog} alt="tech blog" />
+          <h2>Movie Comparison Site</h2>
+        </a>
+      </div>
 
 
     </div>
